5bded09d33feda40890806cd4bab01b94e568dbe
André Corrêa <andreclinic@gmail.com>
2021-08-09 17:03:59 -0300

    app.js

---
+++ app-multiple-device.js
@@ -0,0 +1,35 @@
+id = emailaddress
+id = password
+
+button  - btn btn-primary btn-block
+
+
+document.getElementById('emailaddress').value = "andre@gmmail.com";
+
+
+FTP user profile
+
+User：ftp_wa_alsweb_com_br
+
+Password：WY8KCA66KEdc4LGe
+
+Database user profile
+
+Database name：sql_wa_alsweb_co
+
+User：sql_wa_alsweb_co
+
+Password：e2kDBf88Am4c6k5L
+
+
+
+
+Database account info
+
+Database Name: wa_alsweb_com_b
+
+User: wa_alsweb_com_b
+
+Password: pipNTRNHCm
+
+Access Site: http://wa.alsweb.com.br
